edebfb14359465f0807fc41d808d7aa88be120f2
gabrielrez <gabrielrezcpessoa@gmail.com>
2025-03-02 19:29:57 -0300

    feat: estruturação das rotas principais

---
+++ BookNest/app/Http/Controllers/BookController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        // Retornar livros da biblioteca do usuário autenticado
+    }
+
+    public function show()
+    {
+        // Retornar um livro específico da biblioteca do usuário autenticado
+    }
+
+    public function store()
+    {
+        // Adiciona livro na biblioteca do usuário autenticado
+    }
+
+    public function delete()
+    {
+        // Deleta um livro específico na biblioteca do usuário autenticado
+    }
+}
--- BookNest/routes/api.php
+++ BookNest/routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\NotesController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/users/register', [UsersController::class, 'register']);
+Route::post('/users/login', [UsersController::class, 'login']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{$id}', [BookController::class, 'show']);
+Route::post('/books', [BookController::class, 'store']);
+Route::delete('/books/{id}', [BookController::class, 'delete']);
+
+Route::get('/books/{id}/notes', [NotesController::class, 'index']);
+Route::get('/books/{id}/notes/{id}', [NotesController::class, 'show']);
+Route::post('/books/{id}/notes', [NotesController::class, 'store']);
+Route::delete('/books/{id}/notes/{id}', [NotesController::class, 'delete']);
